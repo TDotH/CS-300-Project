6b77b104807ab0618d73368cda80d49eb15f2300
TDotH <hasht795@hotmail.com>
2020-01-23 09:00:23 -0800

    Base Game File

---
+++ Frupal.java
@@ -0,0 +1,32 @@
+//import java.awt.*;
+
+import javax.swing.JFrame;
+//import javax.swing.JPanel;
+
+//TODO: Create initialization method
+
+public class Frupal {
+
+	
+	static final int SCREEN_WIDTH = 800;
+	static final int SCREEN_HEIGHT = 600;
+	
+	//main game loop
+	public static void main( String[] args ) {
+		
+		JFrame frame = new JFrame("Frupal vAlpha");
+		frame.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+		frame.setVisible( true );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		boolean gameOn = true;
+		
+		//Main game loop
+		while( gameOn ) {
+			//Do stuff
+			
+		}
+		
+		
+	}
+}
